3e15cd9243cadec0c047fe5d4b4d3d75a0c01dd7
Varun Vinay <45584312+varunvinay4u@users.noreply.github.com>
2022-07-13 12:11:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-auth=getAuth(app);
+const auth=getAuth(app);
 
 onAuthStateChanged(auth,user➡{
     if(user=null){
