a1732fd2a6b3352468bd590ced716e4e861bd89f
Swathi K <swathikrmt@gmail.com>
2022-07-13 20:22:11 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,66 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getAuth, onAuthStateChanged} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+        $("nav ul").toggleClass("showing");
+  });
+});
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBejYGfKipqW-T_0gfNoh609oZH9WL6hyk",
-  authDomain: "pg19cs10.firebaseapp.com",
-  projectId: "pg19cs10",
-  storageBucket: "pg19cs10.appspot.com",
-  messagingSenderId: "82975153140",
-  appId: "1:82975153140:web:702e32ee2341d0e0d537a1",
-  measurementId: "G-0D0WD65KQT"
-};
+// Scrolling Effect
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth=getAuth(app);
+$(window).on("scroll", function() {
+  if($(window).scrollTop()) {
+        $('nav').addClass('black');
+  }
 
-onAuthStateChanged(auth,user➡{
-    if(user=null){
-     console.log('logged in!');
-    }else {
-      console.log('No user');
-   }
+  else {
+        $('nav').removeClass('black');
+  }
+})
 
 
-var navLinks = document.getElementById("navLinks");
-function showMenu(){
-    navLinks.style.right = "0";
-}
-function hideMenu(){
-    navLinks.style.right = "-200px";
-}
+
+
+
+
+
+
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import {getAuth, onAuthStateChanged} from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBejYGfKipqW-T_0gfNoh609oZH9WL6hyk",
+//   authDomain: "pg19cs10.firebaseapp.com",
+//   projectId: "pg19cs10",
+//   storageBucket: "pg19cs10.appspot.com",
+//   messagingSenderId: "82975153140",
+//   appId: "1:82975153140:web:702e32ee2341d0e0d537a1",
+//   measurementId: "G-0D0WD65KQT"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const auth=getAuth(app);
+
+// onAuthStateChanged(auth,user➡{
+//     if(user=null){
+//      console.log('logged in!');
+//     }else {
+//       console.log('No user');
+//    }
+
+
+// var navLinks = document.getElementById("navLinks");
+// function showMenu(){
+//     navLinks.style.right = "0";
+// }
+// function hideMenu(){
+//     navLinks.style.right = "-200px";
+// }
