b37b04225eb48c209213aeba8cbc51a60eb05d11
varunvinay4u <varunvinay4u@gmail.com>
2022-07-26 02:20:58 +0530

    drop down value fetch fixed

--- assets/signup.php
+++ assets/signup.php
@@ -101,7 +101,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 		//something was posted
 		$name =$_POST['name'];
 		$password = $_POST['password'];
-   	$category=$_POST['category'];
+   	$category=$_POST['register_as'];
 		$email=$_POST['email'];
 		$phoneNumber=$_POST['phoneNumber'];
 		$gender=$_POST['gender'];
@@ -115,7 +115,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 
 			//save to database
 			$user_id = random_num(20);
-			$sql = "insert into login values ('',$user_id,'doctor','$name','$email','$password',$phoneNumber,'$gender',$zipcode,'$location','$Qualification','$DoctorRegistrationID')";
+			$sql = "insert into login values ('',$user_id,'$category','$name','$email','$password',$phoneNumber,'$gender',$zipcode,'$location','$Qualification','$DoctorRegistrationID')";
         //check if $category works in insert or not !!NOT
 			
 			if ($con->query($sql) === TRUE) {
