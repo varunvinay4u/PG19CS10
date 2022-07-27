2d649834efc1097397f761b249d0e0c4a01a4cc8
SREEHARI-P-S <68457346+SREEHARI-P-S@users.noreply.github.com>
2022-07-27 10:30:25 +0530

    Update legal.php

--- assets/legal.php
+++ assets/legal.php
@@ -44,7 +44,10 @@
       OVERVIEW
       This is a legally binding document governing the provision of the Mind Buddy our website.
 
-      When you sign-up for an Account, You automatically accept the below provisions which constitute the Mind Buddy Terms and Conditions and You are legally bound by them. 
+ When you sign-up for an Account, You automatically accept the below provisions which constitute the Mind Buddy Terms and Conditions and You are legally bound by them. 
+Read this document very carefully and together with our Website Terms and Conditions, Mind Buddy Privacy Policy and Data Processing Agreement.
+    If you are an Enterprise Client, you accept the provisions herein and any additional documentation tailored to your needs. Get in contact with us to find out more.
+    IF YOU DO NOT ACCEPT ANY OF THE PROVISIONS BELOW, DO NOT USE OUR SERVICES.
     </p> <br><br>
     <div onload="disableSubmit()">
       <input type="checkbox" name="terms" id="terms" onchange="activateButton(this)">  I Agree Terms & Coditions <br><br>
